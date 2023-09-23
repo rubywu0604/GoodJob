47a7bda22ed07b9ae063b88912c78d61c2ae21f3
rubywu0604 <lksh20602@gmail.com>
2023-09-24 01:40:55 +0800

    feat: create category search api

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const http = require('http');
+// ================= node package / middleware =================
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const path = require('path');
+const port = 8080;
 const mysql = require('mysql');
 
-require('dotenv').config();
-const host = process.env.RDS_HOSTNAME;
-const port = process.env.RDS_PORT;
-const user = process.env.RDS_USERNAME;
-const password = process.env.RDS_PASSWORD;
+// ======================== Database ========================
+const db_host = process.env.RDS_HOSTNAME;
+const db_port = process.env.RDS_PORT;
+const db_user = process.env.RDS_USERNAME;
+const db_password = process.env.RDS_PASSWORD;
+const db_database = process.env.RDS_DATABASE;
+
 const db = mysql.createConnection({
-    host: host,
-    port: port,
-    user: user,
-    password: password
+    host: db_host,
+    port: db_port,
+    user: db_user,
+    password: db_password,
+    database: db_database
 })
 
 function connectToMysql(db) {
@@ -32,10 +40,40 @@ function createDatabase(db) {
         console.log("Database created.");
     });
 }
-
 connectToMysql(db)
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World');
-}).listen(8080);
+// ================= API routes =================
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
+
+app.get('/api/jobs', (req, res) => {
+    db.query('SELECT * FROM job', (err, results) => {
+        if (err) {
+            console.log(err.message);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.get(`/api/jobs/:category`, (req, res) => {
+    const category = req.params.category;
+    const query = 'SELECT * FROM job WHERE category = ?';
+    db.query(query, [category], (err, results) => {
+        if (err) {
+            console.log(err.message);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(results);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listen on the port ${port}`);
+});
