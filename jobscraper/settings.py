fd1e5b58fc20012c41129bf07f5390f35ad41cb0
rubywu0604 <lksh20602@gmail.com>
2023-10-09 05:08:52 +0800

    feat: import boto3 botocore

--- jobscraper/settings.py
+++ jobscraper/settings.py
@@ -20,12 +20,15 @@ SCRAPEOPS_API_KEY = "50ee86a1-5ea9-4db7-9d49-fd50a85be177"
 AWS_ACCESS_KEY_ID = "AKIA6ANQNG7AR3RUT3OS"
 AWS_SECRET_ACCESS_KEY = "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"
 
-today = datetime.datetime.today() 
+today = datetime.datetime.today()
+
 FEEDS = {
    f'backupdata{today.strftime("%Y/%m-%d")}.json': {'format': 'json'},
    f's3://project-goodjob/{today.strftime("%Y-%m-%d")}/%(name)s_%(time)s.jsonl': {
-      "format": "jsonlines",
-      }
+      'format': 'jsonlines',
+      'store_empty': False,
+      'encoding': 'utf8', 
+   }
 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -75,13 +78,13 @@ DOWNLOADER_MIDDLEWARES = {
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
 EXTENSIONS = {
    "scrapeops_scrapy.extension.ScrapeOpsMonitor": 500,
-   # "scrapy.extensions.telnet.TelnetConsole": None,
+   "scrapy.extensions.feedexport": None,
 }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "jobscraper.pipelines.JobscraperPipeline": 300,
+   "jobscraper.pipelines.JobscraperPipeline": 300
 }
 ITEM_ADAPTER = 'jobscraper.adapters.CustomItemAdapter'
 
@@ -109,4 +112,3 @@ HTTPCACHE_DIR = "httpcache"
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
