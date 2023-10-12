3d4356812ab0b11f80ae59cbc2fcc3989869bcad
rubywu0604 <lksh20602@gmail.com>
2023-10-13 03:22:52 +0800

    feat: modify job title and salary in job list

--- public/script.js
+++ public/script.js
@@ -23,21 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
             const experienceCounts = {};
 
             data.forEach((job) => {
-                category = job.category;
+                let category = job.category;
                 category = category.replace("_", " ");
                 if (category === "ios engineer") {
-                    category = "iOS Engineer";
-                } else {
-                    category = category.replace(/\b\w/g, function (char) {
-                        return char.toUpperCase();
-                    });
+                    category = "iOS Engineer"
+                } else if (category === "dba") {
+                    category = "DBA"
+                }else {
+                    category = category.replace(/\b\w/g, (char) => char.toUpperCase());
                 }
-
+                (jobDetails[category].counts === 0) ? jobDetails[category].counts = 1 : jobDetails[category].counts++;
+                
                 const minMonthlySalary = job.min_monthly_salary;
                 const maxMonthlySalary = job.max_monthly_salary;
                 let experience = job.experience;
-
-                (category === "Dba Engineer") ? jobDetails['Dba'].counts++ : jobDetails[category].counts++;
+                
                 (experience === 0) ? (experience = "不拘") : (experience = `${experience}年`);
                 experienceCounts.hasOwnProperty(experience) ? experienceCounts[experience]++ : (experienceCounts[experience] = 1);
 
@@ -191,8 +191,7 @@ function createJobList(data) {
 
     let tbody = document.createElement('tbody');
     data.forEach((job, index) => {
-        let job_title = (job.job_title.includes("工程師") ? job.job_title.slice(0, job.job_title.indexOf("工程師") + 3) : job.job_title.slice(0, 20));
-        job_title = (job_title.length > 40) ? job_title.slice(0, 40) : job_title;
+        let job_title = (job.job_title.length > 50) ? job.job_title.slice(0, 50) + "..." : job.job_title;
         let job_link = job.job_link;
         let region = job.location;
         let companyRegex = /[\u4e00-\u9fa5]+公司/;
@@ -218,9 +217,17 @@ function createJobList(data) {
         let td3 = document.createElement('td');
         let td4 = document.createElement('td');
         td2.textContent = region;
+        td2.style.textAlign = 'center';
         td3.textContent = company;
         td3.style.textAlign = 'center';
-        td4.textContent = (minMonthlySalary === "Null" || maxMonthlySalary === "Above") ? "面議" : `${minMonthlySalary} ～ ${maxMonthlySalary}`
+        if (minMonthlySalary === "Null") {
+            td4.textContent = "面議(40,000以上)";
+        } else if (maxMonthlySalary === "Above") {
+            td4.textContent = `面議($${parseInt(minMonthlySalary).toLocaleString()}以上)`;
+        } else {
+            td4.textContent = `$${parseInt(minMonthlySalary).toLocaleString()} ～ $${parseInt(maxMonthlySalary).toLocaleString() }`
+        }
+        td4.style.textAlign = 'center';
 
         tr.appendChild(td1);
         tr.appendChild(td2);
