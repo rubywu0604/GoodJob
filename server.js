6b66a33a20b3578d589d1e1d55d6def93c29c259
rubywu0604 <lksh20602@gmail.com>
2023-10-17 19:21:11 +0800

    feat: defined dev/prod port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./database.js')
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 8080;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
